9cf5e2de426979e300dfdca2ecdfa8d5c258c457
JINTO N SEBASTIAN <65025344+jinto4638@users.noreply.github.com>
2021-08-14 21:51:51 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -81,10 +81,10 @@ async def filter(client, message):
             if API_KEY:
                 poster=await get_poster(search)
             if poster:
-                await message.reply_photo(photo=poster, caption=f"<b>Here is What I Found In My Database For Your Query</b> `{search}` ‌‌‌‌‎ ­  ­  ­­\n **For more accuracy MOVIE NAME YEAR LANGUAGE**  ­  ­ ", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_photo(photo=poster, caption=f"<b>Here is What I Found In My Database For Your Query</b> 👉 `{search}` ‌‌‌‌‎ ­  ­  ­­\n **For more accuracy** \n **MOVIE NAME YEAR LANGUAGE**  ­  ­ ", reply_markup=InlineKeyboardMarkup(buttons))
 
             else:
-                await message.reply_text(f"<b>Here is What I Found In My Database For Your Query </b> `{search}` ‎ ­  ­ ­\n **For more accuracy MOVIE NAME YEAR LANGUAGE** ­  ­  ­  ", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_text(f"<b>Here is What I Found In My Database For Your Query </b>  👉 `{search}` ‎ ­  ­ ­\n **For more accuracy** \n **MOVIE NAME YEAR LANGUAGE** ­  ­  ­  ", reply_markup=InlineKeyboardMarkup(buttons))
             return
 
         data = BUTTONS[keyword]
@@ -100,9 +100,9 @@ async def filter(client, message):
         if API_KEY:
             poster=await get_poster(search)
         if poster:
-            await message.reply_photo(photo=poster, caption=f"<b>Here is What I Found In My Database For Your Query</b> `{search}` ‌‌‌‌‎ ­\n **For more accuracy MOVIE NAME YEAR LANGUAGE**  ­  ­  ­  ­  ", reply_markup=InlineKeyboardMarkup(buttons))
+            await message.reply_photo(photo=poster, caption=f"<b>Here is What I Found In My Database For Your Query</b>  👉 `{search}` ‌‌‌‌‎ ­\n **For more accuracy**\n **MOVIE NAME YEAR LANGUAGE**  ­  ­  ­  ­  ", reply_markup=InlineKeyboardMarkup(buttons))
         else:
-            await message.reply_text(f"<b>Here is What I Found In My Database For Your Query</b> `{search}` ‌‌‌‌‎ ­­\n **For more accuracy MOVIE NAME YEAR LANGUAGE**  ­  ­  ­  ­ ", reply_markup=InlineKeyboardMarkup(buttons))
+            await message.reply_text(f"<b>Here is What I Found In My Database For Your Query</b>  👉 `{search}` ‌‌‌‌‎ ­­\n **For more accuracy**\n **MOVIE NAME YEAR LANGUAGE**  ­  ­  ­  ­ ", reply_markup=InlineKeyboardMarkup(buttons))
 
 @Client.on_message(filters.text & filters.group & filters.incoming & filters.chat(AUTH_GROUPS) if AUTH_GROUPS else filters.text & filters.group & filters.incoming)
 async def group(client, message):
@@ -145,9 +145,9 @@ async def group(client, message):
             if API_KEY:
                 poster=await get_poster(search)
             if poster:
-                await message.reply_photo(photo=poster, caption=f"<b>Here is What I Found In My Database For Your Query {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_photo(photo=poster, caption=f"<b>Here is What I Found In My Database For Your Query</b>  👉 `{search}` ‌‌‌‌‎ ­­\n **For more accuracy**\n **MOVIE NAME YEAR LANGUAGE** ‌‌‌‌‎ ­  ­  ­  ­  ­  ", reply_markup=InlineKeyboardMarkup(buttons))
             else:
-                await message.reply_text(f"<b>Here is What I Found In My Database For Your Query {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_text(f"<b>Here is What I Found In My Database For Your Query</b>  👉 `{search}` ‌‌‌‌‎ ­­\n **For more accuracy**\n **MOVIE NAME YEAR LANGUAGE**  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
             return
 
         data = BUTTONS[keyword]
@@ -163,9 +163,9 @@ async def group(client, message):
         if API_KEY:
             poster=await get_poster(search)
         if poster:
-            await message.reply_photo(photo=poster, caption=f"<b>Here is What I Found In My Database For Your Query</b> `{search}` ‌‌‌‌‎ ­  ­  ­  ­\n **For more accuracy MOVIE NAME YEAR LANGUAGE**­  ­  ", reply_markup=InlineKeyboardMarkup(buttons))
+            await message.reply_photo(photo=poster, caption=f"<b>Here is What I Found In My Database For Your Query</b>  👉 `{search}` ‌‌‌‌‎ ­­\n **For more accuracy**\n **MOVIE NAME YEAR LANGUAGE**     ­  ", reply_markup=InlineKeyboardMarkup(buttons))
         else:
-            await message.reply_text(f"<b>Here is What I Found In My Database For Your Query</b> `{search}` ‌‌‌‌‎ ­  ­  ­  ­­\n **For more accuracy MOVIE NAME YEAR LANGUAGE**  ­  ", reply_markup=InlineKeyboardMarkup(buttons))
+            await message.reply_text(f"<b>Here is What I Found In My Database For Your  Query</b>  👉 `{search}` ‌‌‌‌‎ ­­\n **For more accuracy**\n **MOVIE NAME YEAR LANGUAGE**   ­  ", reply_markup=InlineKeyboardMarkup(buttons))
 
     
 def get_size(size):
