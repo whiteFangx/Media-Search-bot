5e4656e88d0f3e2c3ef305bb16a3c5a169011756
Hc787088 <98957345+Hc787088@users.noreply.github.com>
2022-11-15 18:43:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -6,9 +6,9 @@ id_pattern = re.compile(r'^.\d+$')
 # Bot information
 SESSION = 'LeoMediaSearchBot'
 USER_SESSION = 'User_Bot'
-API_ID = 1706730
-API_HASH = '14a483d10b9191f077e1a954a131c59e'
-BOT_TOKEN = '5690605277:AAHYoufns6U8XiYIQP6kSYfIcnYCFV5cfN8'
+API_ID = 4639219
+API_HASH = '8aa6ff17490134bf275616030f46e29e'
+BOT_TOKEN = '5131802526:AAHS1NLVo1Qyxo3kT1t1L1NX-spo7a7SHJA'
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -16,15 +16,15 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 
 # Admins, Channels & Users
 ADMINS = ['@ravi00893']
-CHANNELS = [-1001339472860]
+CHANNELS = [-1001538461400]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = []
 auth_channel = environ.get('AUTH_CHANNEL')
-AUTH_CHANNEL = -1001814578073
+AUTH_CHANNEL = -1001531025479
 AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
 
 # MongoDB information
-DATABASE_URI = "mongodb+srv://cinee:cinee@cluster0.nr6ekfd.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_URI = " mongodb+srv://jns4638:jns4638@cluster0.qyh3o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 DATABASE_NAME = 'cinee'
 COLLECTION_NAME = 'channel_files'
 
